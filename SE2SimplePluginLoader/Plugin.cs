7313684666be70155da43562164b6397364e97ed
WesternGamer <Kevnguyen474@gmail.com>
2025-01-29 21:18:57 -0500

    Add project files.

---
+++ SE2SimplePluginLoader/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using Keen.Game2.Game.Plugins;
+using Keen.VRage.Core;
+using Keen.VRage.Library.Diagnostics;
+using Keen.VRage.Library.Utils;
+
+namespace SE2SimplePluginLoader
+{
+    public class Plugin : IPlugin, IDisposable
+    {
+        public const string Name = "SE2SimplePluginLoader";
+
+        private List<PluginInstance> plugins = new List<PluginInstance>();
+
+        public Plugin(PluginHost host)
+        {
+            Log.Default.Info($"[{Name}] Loading plugins.");
+            foreach(string plugin in Directory.EnumerateFiles(Path.Combine(Singleton<VRageCore>.Instance.AppDataPath, "Plugins"), "*.dll"))
+            {
+                if (plugin.Contains("SE2SimplePluginLoader.dll") || plugin.Contains("0Harmony.dll"))
+                {
+                    continue;
+                }
+
+                if (PluginInstance.Create(plugin, out PluginInstance instance))
+                {
+                    plugins.Add(instance);
+                }
+            }
+
+            foreach(PluginInstance instance in plugins)
+            {
+                instance.LoadPlugin(host);
+            }
+
+            Log.Default.Info($"[{Name}] Finished loading plugins.");
+        }
+
+        public void Dispose()
+        {
+            Log.Default.Info($"[{Name}] Unloading plugins.");
+
+            foreach (PluginInstance instance in plugins)
+            {
+                instance.Dispose();
+            }
+
+            plugins.Clear();
+            Log.Default.Info($"[{Name}] Unloaded plugins.");
+        }
+    }
+}
---
+++ SE2SimplePluginLoader/PluginInstance.cs
@@ -0,0 +1,56 @@
+﻿using Keen.Game2.Game.Plugins;
+using Keen.VRage.Library.Diagnostics;
+using System.Reflection;
+
+namespace SE2SimplePluginLoader
+{
+    internal class PluginInstance
+    {
+        private readonly Type entryPointType;
+        private IPlugin pluginInstance;
+
+        private PluginInstance(Type entryPointType) 
+        {
+            this.entryPointType = entryPointType;
+        }
+
+        public void LoadPlugin(PluginHost host)
+        {
+            try
+            {
+                pluginInstance = (IPlugin)Activator.CreateInstance(entryPointType, new object[] { host });
+            }
+            catch (MissingMethodException)
+            {
+                pluginInstance = (IPlugin)Activator.CreateInstance(entryPointType);
+            }
+        }
+
+        public void Dispose()
+        {
+            if ((pluginInstance as IDisposable) != null)
+            {
+                (pluginInstance as IDisposable).Dispose();
+            }
+        }
+
+        public static bool Create(string assemblyPath, out PluginInstance pluginInstance)
+        {
+            pluginInstance = null;
+
+            Assembly assembly = Assembly.LoadFile(assemblyPath);
+
+            Type entryPointType = assembly.GetTypes().FirstOrDefault(t => typeof(IPlugin).IsAssignableFrom(t));
+
+            if (entryPointType == null)
+            {
+                Log.Default.Error($"[{Plugin.Name}]Skipping {assemblyPath} as it does not have a IPlugin");
+                return false;
+            }
+
+            pluginInstance = new PluginInstance(entryPointType);
+            return true;
+        }
+       
+    }
+}
